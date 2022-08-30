d12f0d0581c18904b0e269e4340bf49924f85b6d
Ajay Dandge <60701544+er-knight@users.noreply.github.com>
2022-08-30 20:09:19 +0530

    add build instructions

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ -e build ]; then rm -rf build; fi
+if [ -e dist ]; then rm -rf dist; fi
+if [ -e contests.spec ]; then rm contests.spec; fi
+if [ -e contests.zip ]; then rm contests.zip; fi
+
+if [ -e "venv" ]
+then
+    source venv/bin/activate
+else
+    echo "venv not found"
+    exit
+fi
+
+python -m PyInstaller contests.py >/dev/null 2>&1
+
+cd dist && zip -rq9 contests.zip contests && mv contests.zip ..
